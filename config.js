2ebc50d1e76aca069214d7f6305dbe16625aa81f
moslem <moslem.ezati@gmail.com>
2021-01-29 10:07:39 +0330

    Added Twilio send sms service

--- config.js
+++ config.js
@@ -3,12 +3,18 @@ var environments = {};
 environments.development = {
     port: 3000,
     secretKey: 'silence is our ammunition!',
-    maxChecks: 5
+    maxChecks: 5,
+    twilio: {
+        'accountSid': 'ACb32d411ad7fe886aac54c665d25e5c5d',
+        'authToken': '9455e3eb3109edc12e3d8c92768f7a67',
+        'fromPhone': '+15005550006'
+    }
 }
 environments.production = {
     port: 5000,
     secretKey: 'lie is a standard procedure!',
-    maxChecks: 5
+    maxChecks: 5,
+    twilio: {}
 }
 
 var environmentName = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
