50ce686ae4d09f20ecd80e14c31abf1f71a86188
moslem <moslem.ezati@gmail.com>
2021-02-11 18:38:09 +0330

    Added  interpolate function and footer and header template

--- config.js
+++ config.js
@@ -8,13 +8,25 @@ environments.development = {
         'accountSid': 'ACb32d411ad7fe886aac54c665d25e5c5d',
         'authToken': '9455e3eb3109edc12e3d8c92768f7a67',
         'fromPhone': '+15005550006'
+    },
+    templateGlobals: {
+        appName: 'UptimeChecker',
+        companyName: 'NotARealCompany, Inc.',
+        yearCreated: '2018',
+        baseUrl: 'http://localhost:3000/'
     }
 }
 environments.production = {
     port: 5000,
     secretKey: 'lie is a standard procedure!',
     maxChecks: 5,
-    twilio: {}
+    twilio: {},
+    templateGlobals: {
+        appName: 'UptimeChecker',
+        companyName: 'NotARealCompany, Inc.',
+        yearCreated: '2018',
+        baseUrl: 'http://localhost:5000/'
+    }
 }
 
 var environmentName = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
