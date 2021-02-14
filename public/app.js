f1f694812134866775322734034b82f2fb2784f4
moslem <moslem.ezati@gmail.com>
2021-02-14 21:15:19 +0330

    Added XHR request method in front-end

--- public/app.js
+++ public/app.js
@@ -1,1 +1,64 @@
-console.log('Hello World');
+/*
+ * Frontend Logic for application
+ *
+ */
+
+var app = {};
+
+app.config = {
+    'sessionToken': false
+};
+
+app.client = {}
+
+app.client.request = function (headers, path, method, queryStringObject, payload, callback) {
+
+    // Set defaults
+    headers = typeof (headers) == 'object' && headers !== null ? headers : {};
+    path = typeof (path) == 'string' ? path : '/';
+    method = typeof (method) == 'string' && ['POST', 'GET', 'PUT', 'DELETE'].includes(method.toUpperCase()) ? method.toUpperCase() : 'GET';
+    queryStringObject = typeof (queryStringObject) == 'object' && queryStringObject !== null ? queryStringObject : {};
+    payload = typeof (payload) == 'object' && payload !== null ? payload : {};
+    callback = typeof (callback) == 'function' ? callback : false;
+
+    let queryString = new URLSearchParams();
+    for (var queryKey in queryStringObject) {
+        queryString.append(queryKey, queryStringObject[queryKey]);
+    }
+    let requestUrl = path;
+    if (queryString.length > 0) {
+        requestUrl += queryString;
+    }
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, requestUrl, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    for (var headerKey in headers) {
+        if (headers.hasOwnProperty(headerKey)) {
+            xhr.setRequestHeader(headerKey, headers[headerKey]);
+        }
+    }
+    
+    if (app.config.sessionToken) {
+        xhr.setRequestHeader("token", app.config.sessionToken.id);
+    }
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var statusCode = xhr.status;
+            var responseReturned = xhr.responseText;
+
+            if (callback) {
+                try {
+                    var parsedResponse = JSON.parse(responseReturned);
+                    callback(statusCode, parsedResponse);
+                } catch (e) {
+                    callback(statusCode, false);
+                }
+            }
+        }
+    }
+
+    var payloadString = JSON.stringify(payload);
+    xhr.send(payloadString);
+};
