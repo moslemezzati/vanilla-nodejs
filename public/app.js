8eb59a6c5afa2c3af7ce8dae3d0076902ad39013
moslem <moslem.ezati@gmail.com>
2021-02-15 18:53:34 +0330

    Added created account in front-end

--- public/app.js
+++ public/app.js
@@ -38,7 +38,7 @@ app.client.request = function (headers, path, method, queryStringObject, payload
             xhr.setRequestHeader(headerKey, headers[headerKey]);
         }
     }
-    
+
     if (app.config.sessionToken) {
         xhr.setRequestHeader("token", app.config.sessionToken.id);
     }
@@ -61,4 +61,56 @@ app.client.request = function (headers, path, method, queryStringObject, payload
 
     var payloadString = JSON.stringify(payload);
     xhr.send(payloadString);
+};
+
+
+
+app.bindForms = function () {
+    document.querySelector("form").addEventListener("submit", function (e) {
+        e.preventDefault();
+        var formId = this.id;
+        var path = this.action;
+        var method = this.method.toUpperCase();
+
+        document.querySelector("#" + formId + " .formError").style.display = 'hidden';
+
+        var payload = {};
+        var elements = this.elements;
+        for (var i = 0; i < elements.length; i++) {
+            if (elements[i].type !== 'submit') {
+                var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
+                payload[elements[i].name] = valueOfElement;
+            }
+        }
+
+        app.client.request(undefined, path, method, undefined, payload, function (statusCode, responsePayload) {
+            if (statusCode !== 200) {
+
+                var error = typeof (responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
+
+                document.querySelector("#" + formId + " .formError").innerHTML = error;
+
+                document.querySelector("#" + formId + " .formError").style.display = 'block';
+
+            } else {
+                app.formResponseProcessor(formId, payload, responsePayload);
+            }
+
+        });
+    });
+};
+
+app.formResponseProcessor = function (formId, requestPayload, responsePayload) {
+    var functionToCall = false;
+    if (formId == 'accountCreate') {
+        // @TODO Do something here now that the account has been created successfully
+    }
+};
+
+app.init = function () {
+    app.bindForms();
+};
+
+window.onload = function () {
+    app.init();
 };
