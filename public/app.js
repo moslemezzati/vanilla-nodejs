8f251fc1e07640042d4a8679ab20c0344515bfbd
moslem <moslem.ezati@gmail.com>
2021-02-25 18:28:00 +0330

    Added login to get data from form of create check

--- public/app.js
+++ public/app.js
@@ -85,18 +85,42 @@ app.bindForms = function () {
 
             document.querySelector("#" + formId + " .formError").style.display = 'hidden';
 
+            
             var payload = {};
             var elements = this.elements;
             for (var i = 0; i < elements.length; i++) {
                 if (elements[i].type !== 'submit') {
-                    var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
-                    if (elements[i].name == '_method') {
+                    // Determine class of element and set value accordingly
+                    var classOfElement = typeof (elements[i].classList.value) == 'string' && elements[i].classList.value.length > 0 ? elements[i].classList.value : '';
+                    var valueOfElement = elements[i].type == 'checkbox' && classOfElement.indexOf('multiselect') == -1 ? elements[i].checked : classOfElement.indexOf('intval') == -1 ? elements[i].value : parseInt(elements[i].value);
+                    var elementIsChecked = elements[i].checked;
+                    // Override the method of the form if the input's name is _method
+                    var nameOfElement = elements[i].name;
+
+                    if (nameOfElement == 'httpmethod') {
+                        nameOfElement = 'method';
+                    }
+                    if (nameOfElement == '_method') {
                         method = valueOfElement;
                     } else {
-                        payload[elements[i].name] = valueOfElement;
+                        // Create an payload field named "method" if the elements name is actually httpmethod
+                        if (nameOfElement == 'httpmethod') {
+                            nameOfElement = 'method';
+                        }
+                        // If the element has the class "multiselect" add its value(s) as array elements
+                        if (classOfElement.indexOf('multiselect') > -1) {
+                            if (elementIsChecked) {
+                                payload[nameOfElement] = typeof (payload[nameOfElement]) == 'object' && payload[nameOfElement] instanceof Array ? payload[nameOfElement] : [];
+                                payload[nameOfElement].push(valueOfElement);
+                            }
+                        } else {
+                            payload[nameOfElement] = valueOfElement;
+                        }
+
                     }
                 }
             }
+
             var queryStringObject = method == 'DELETE' ? payload : {};
             app.client.request(undefined, path, method, queryStringObject, payload, function (statusCode, responsePayload) {
                 if (statusCode !== 200 && statusCode !== 201 && statusCode !== 204) {
